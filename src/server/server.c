09cfa5a1ab4c4365d268550bc64ef352dbcb6cd5
StartaBafras <beyhankaradag00@gmail.com>
2021-07-07 10:53:14 +0300

    Rewritten using getaddrinfo()

--- src/server/server.c
+++ src/server/server.c
@@ -5,6 +5,8 @@
 #include <netinet/in.h>
 #include <string.h>
 
+#include <netdb.h> //addrinfo
+
 #define PORT 3979
 
 int main(void)
@@ -13,7 +15,17 @@ int main(void)
     int sockfd;
     struct sockaddr_in address;
 
+    struct addrinfo hints; //hints basically determines which sockets get to be in res.
+    struct addrinfo *res; //sockets we get from getaddrinfo, restricted by the attributes we specified in hints.
+
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; //We only want IPV4 sockets in res. So we specify this in hints. Use AF_INET6 for IPV6 and AF_UNSPEC for both.
+    hints.ai_socktype = SOCK_STREAM;
+
     sockfd = socket(AF_INET,SOCK_STREAM,0);
+
+    getaddrinfo(NULL, "3979" , &hints, &res);
 
     if(sockfd == 0)
     {
@@ -25,13 +37,18 @@ int main(void)
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    bind(sockfd, (struct sockaddr *)&address, sizeof(address));
+    //bind(sockfd, (struct sockaddr *)&address, sizeof(address));
+    bind(sockfd,res->ai_addr,res->ai_addrlen);
 
     listen(sockfd,5);
 
     int addrlen = sizeof(address);
+    struct sockaddr_storage their_addr;
+    unsigned int addr_size;
 
-    int new_door = accept(sockfd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+
+    //int new_door = accept(sockfd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+    int new_door = accept(sockfd, (struct sockaddr*) &their_addr, &addr_size);
 
     char tmp[256] = {0};
 
